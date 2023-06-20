6f4b8c8d32565fca5736093f0b406ce029038317
andreluis29 <andre.wenningkamp1@gmail.com>
2023-06-20 19:31:12 -0300

    chore: added new files

---
+++ main.js
@@ -0,0 +1,28 @@
+const userName = document.querySelector("#name");
+const userTelephone = document.querySelector("#telephone");
+const btnSubmit = document.querySelector("button");
+const userRow = document.querySelector(".userRow");
+
+function verifyInput(input) {
+  console.log(input.value);
+  if (input.value !== "") {
+    return true;
+  }
+  alert(`Erro: Você não digitou o campo ${input.name}`);
+  return false;
+}
+
+function insertName(name, userTelephone) {
+  const verified = verifyInput(name);
+  if (verified) {
+    const verified = verifyInput(userTelephone);
+    if (verified) {
+      userRow.innerHTML += `<tr><td>${name.value}</td><td>${userTelephone.value}</td></tr>`;
+    }
+  }
+}
+
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  insertName(userName, userTelephone);
+});
